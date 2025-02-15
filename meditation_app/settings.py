828ee2d19aa05a13c3ef833070aaae20af227b24
WARMLIGHTUA <meditaciya4@gmail.com>
2025-02-16 01:52:38 +0200

    новлено налаштування AWS S3: змінено ACL на public-read та додано параметри кешування

--- meditation_app/settings.py
+++ meditation_app/settings.py
@@ -255,7 +255,7 @@ if not DEBUG:
     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'eu-north-1')
     AWS_S3_FILE_OVERWRITE = False
-    AWS_DEFAULT_ACL = None
+    AWS_DEFAULT_ACL = 'public-read'
     AWS_S3_VERIFY = True
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     
